d6c4485c5faba6ac94b6a55f7c572a0a69a922ef
Kaveesha Shewon <shewon.ksh123@gmail.com>
2026-08-21 15:07:30 +0530

    Bump app version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "lk.prime.dailyactivity"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
